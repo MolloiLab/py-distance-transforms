185e8cdafde73c5f5ccbef4d8150db39195c088e
Dale Black <djblack@uci.edu>
2024-05-09 15:23:28 -0700

    try removing CUDA for testing

--- distancetransforms/julia_import.py
+++ distancetransforms/julia_import.py
@@ -1,13 +1,8 @@
 from juliacall import Main as jl
 
-try:
-    jl.seval("using Pkg; Pkg.status()")
-    jl.seval("using DistanceTransforms")
-    jl.seval("using CUDA")
-except Exception as e:
-    print(f"Error: {e}")
-    jl.seval("using Pkg; Pkg.status()")
-    raise e
+jl.seval("using Pkg; Pkg.status()")
+jl.seval("using DistanceTransforms")
+# jl.seval("using CUDA")
 
 DistanceTransforms = jl.DistanceTransforms
 CUDA = jl.CUDA
