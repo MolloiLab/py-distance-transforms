8e01d0c25dbb460cbf6dea3cc554344083b97356
Dale Black <djblack@uci.edu>
2024-05-28 09:59:33 -0700

    fix CUDA import

--- py_distance_transforms/julia_import.py
+++ py_distance_transforms/julia_import.py
@@ -4,7 +4,7 @@ from juliacall import Main as jl
 jl.seval("using Pkg; Pkg.status()")
 jl.seval("using PythonCall")
 jl.seval("using DLPack")
-jl.seval("Using CUDA")
+jl.seval("using CUDA")
 jl.seval("using DistanceTransforms")
 
 PythonCall = jl.PythonCall
