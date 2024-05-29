2bb06f8a33a19bd5e5aa3cf6e8a9100d948c28eb
Dale Black <djblack@uci.edu>
2024-05-29 08:51:10 -0700

    clean up imports based on updated DLPack.jl simplify

--- py_distance_transforms/julia_import.py
+++ py_distance_transforms/julia_import.py
@@ -1,12 +1,9 @@
-import juliacall as jc
 from juliacall import Main as jl
 
 jl.seval("using Pkg; Pkg.status()")
-jl.seval("using PythonCall")
 jl.seval("using DLPack")
 jl.seval("using CUDA")
 jl.seval("using DistanceTransforms")
 
-PythonCall = jl.PythonCall
 DLPack = jl.DLPack
 DistanceTransforms = jl.DistanceTransforms
